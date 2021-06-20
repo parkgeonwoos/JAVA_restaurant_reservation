cadf73f2c56e130fe46c90c74849b612838ffc35
parkgeonwoo <pgw111111@naver.com>
2021-06-20 15:23:45 +0900

    210620 commit

---
+++ eatgo/eatgo/src/main/java/eatgo/eatgo/ParkgeonwooApplication.java
@@ -0,0 +1,13 @@
+package eatgo.eatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkgeonwooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkgeonwooApplication.class, args);
+	}
+
+}
