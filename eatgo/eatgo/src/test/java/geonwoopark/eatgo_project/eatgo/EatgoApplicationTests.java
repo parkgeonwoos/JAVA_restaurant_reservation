0e57cc5964f66b7ac13253a904e2b22d86462754
parkgeonwoo <pgw111111@naver.com>
2021-06-20 15:56:09 +0900

    210620 commit

--- eatgo/eatgo/src/test/java/geonwoopark/eatgo_project/eatgo/EatgoApplicationTests.java
+++ eatgo/eatgo/src/test/java/geonwoopark/eatgo_project/eatgo/EatgoApplicationTests.java
@@ -1,13 +1,13 @@
-package eatgo.eatgo;
+package geonwoopark.eatgo_project.eatgo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ParkgeonwooApplicationTests {
+class EatgoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
